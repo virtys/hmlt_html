bbc47fde736ad7bf16c7d440e86918e73bdbd12f
virtys <sergei.blade14@gmail.com>
2017-02-02 10:55:06 +0300

    gulpfile cofig edited

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 const gulp = require('gulp');
 const stylus = require('gulp-stylus');
-// const connect = require('gulp-connect');
 const plumber = require('gulp-plumber');
 const browserSync = require('browser-sync');
 const runSequence = require('run-sequence');
@@ -12,14 +11,6 @@ const mainBowerFiles = require('main-bower-files');
 const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
 const $ = gulpLoadPlugins();
 const reload = browserSync.reload;
-
-// gulp.task('connect', function() {
-//     connect.server({
-//         root: './dist',
-//         livereload: true,
-//         port: 3030
-//     });
-// });
 
 // Optimize images
 gulp.task('images', () =>
@@ -44,16 +35,23 @@ gulp.task('bowerfiles', () => {
 //copy html files
 gulp.task('copy', () => {
     gulp.src(['./app/*', './app/*.html'])
+        .pipe($.newer('./dist'))
         .pipe(gulp.dest('./dist'))
-        .pipe($.size({title: 'copy'}))
+        .pipe($.size({title: 'copy html'}));
+
+    return gulp.src(['./app/fonts/**/*'])
+        .pipe($.newer('./dist/fonts'))
+        .pipe(gulp.dest('./dist/fonts'))
+        .pipe($.size({title: 'copy fonts'}))
 });
 
 gulp.task('styles', () => {
     gulp.src(['./app/css/*.styl', './app/css/*.css'])
+        .pipe($.newer('./dist/css'))
         .pipe(plumber())
         .pipe($.if('*.styl', $.stylus()))
         .pipe(gulp.dest('./dist/css'))
-        .pipe($.size({title: 'copy'}))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('scripts', () => {
@@ -62,7 +60,7 @@ gulp.task('scripts', () => {
 });
 
 
-gulp.task('serve', ['scripts', 'styles'], () => {
+gulp.task('serve', ['default'], () => {
     browserSync({
         notify: false,
         // Customize the Browsersync console logging prefix
@@ -73,57 +71,16 @@ gulp.task('serve', ['scripts', 'styles'], () => {
         // Note: this uses an unsigned certificate which on first access
         //       will present a certificate warning in the browser.
         // https: true,
-        server: ['.tmp', 'app'],
+        // server: ['.tmp', 'app'],
+        server: './dist',
         port: 3000
     });
 
-    gulp.watch(['app/**/*.html'], reload);
-    gulp.watch(['app/styles/**/*.{styl,css}'], ['styles', reload]);
-    gulp.watch(['app/scripts/**/*.js'], ['scripts', reload]);
+    gulp.watch(['app/**/*.html'], ['copy', reload]);
+    gulp.watch(['app/css/**/*.{styl,css}'], ['styles']);
+    gulp.watch(['app/js/**/*.js'], ['scripts', reload]);
     gulp.watch(['app/img/**/*'], reload);
 });
-
-gulp.task('serve:dist', ['default'], () =>
-    browserSync({
-        notify: false,
-        logPrefix: 'WSK',
-        // Allow scroll syncing across breakpoints
-        scrollElementMapping: ['main', '.mdl-layout'],
-        // Run as an https by uncommenting 'https: true'
-        // Note: this uses an unsigned certificate which on first access
-        //       will present a certificate warning in the browser.
-        // https: true,
-        server: 'dist',
-        port: 3001
-    })
-);
-
-// gulp.task('stylus', function () {
-//     gulp.src('./src/styles/style.styl')
-//         .pipe(plumber())
-//         .pipe(stylus())
-//         .pipe(gulp.dest('./'+$dist+'/css'))
-//         .pipe(connect.reload());
-// });
-//
-// gulp.task('html', function () {
-//     gulp.src('./app/*.html')
-//         .pipe(connect.reload());
-// });
-
-// gulp.task('js', function () {
-//     gulp.src('./app/js/*.js')
-//         .pipe(connect.reload());
-// });
-
-// gulp.task('watch', function () {
-//     gulp.watch(['./app/*.html'], ['html']);
-//     gulp.watch(['./src/styles/*.styl'], ['stylus']);
-//     gulp.watch(['./app/js/*.js'], ['js']);
-// });
-
-// gulp.task('default', ['stylus','connect','watch']);
-// gulp.task('default', ['copy']);
 
 gulp.task('default', ['clean'], cb =>
     runSequence(
